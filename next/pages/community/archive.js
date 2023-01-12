2288e9e7f1b29fe05799ff77b5665357029d85db
Mitchell Barton <mitchell@welikesmall.com>
2023-01-12 08:58:01 -0700

    fix links on community archive

--- next/pages/community/archive.js
+++ next/pages/community/archive.js
@@ -91,7 +91,7 @@ export async function getStaticProps() {
             select: {
               title: true,
               eventType: "page.event_type",
-              description: "page.description.markdown.kirbyText",
+              description: "page.description.kirbyText",
               galleryImages: {
                 query: "page.gallery_images.toFiles",
                 select: {
